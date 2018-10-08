3c69d7001a816f7460756416b4745a4b5445dcd8
praveensinghkmr <praveensingh.kmr@gmail.com>
2018-10-08 20:57:07 +0530

    Implemented logic with dictionary

--- NucleotideCount.cs
+++ NucleotideCount.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 
 public class NucleotideCount
 {
+    private Dictionary<char, int> nucleotides;
     public NucleotideCount(string sequence)
     {
+        nucleotides = new Dictionary<char, int>()
+        {
+            ['A'] = 0,
+            ['C'] = 0,
+            ['G'] = 0,
+            ['T'] = 0
+        };
+        foreach(char nucleotide in sequence)
+        {
+            if (nucleotides.ContainsKey(nucleotide))
+                nucleotides[nucleotide] += 1;
+            else
+                throw new ArgumentException();
+        }
     }
 
     public IDictionary<char, int> NucleotideCounts
     {
         get
         {
-            throw new NotImplementedException("You need to implement this function.");
+            return nucleotides;
         }
     }
 }
